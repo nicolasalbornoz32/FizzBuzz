6c9d62ea8d70fc69c06906227e6a48d43bb234e0
Nicolas <nicolasalbornoz32@gmail.com>
2018-10-15 07:34:21 -0300

    version 1.2 (estable)

--- Fizz.js
+++ Fizz.js
@@ -3,22 +3,21 @@ var divisible = false;
 
 for(i = 1; i <= 100; i++)
 {
-    divisible = false;
-    if(i % 3 == 0)
+    if(esDivisible(i,3))
     {
         document.write("fizz");
-        divisible = true;
     }
-    if(i % 5 == 0)
+
+    if(esDivisible(i,5))
     {
         document.write("buzz");
-        divisible = true;
     }
-    if(!divisible)
+
+    if(!esDivisible(i,3) && !esDivisible(i,5))
     {
         document.write(i);
     }
-    document.write("<br />")
+    document.write("<br />");
 }
 
 function esDivisible(num, divisor)
@@ -28,6 +27,6 @@ function esDivisible(num, divisor)
         return true;
     }
     else {
-        return
+        return false;
     }
 }
